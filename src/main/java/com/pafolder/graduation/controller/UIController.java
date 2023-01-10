5c983f9d1f49b1b1dcdc3fda9433aaf8237ec0be
SP <pafolder@gmail.com>
2023-01-10 18:17:16 +0500

    Adds Invalid Username and Password error handling on login page

--- src/main/java/com/pafolder/graduation/controller/UIController.java
+++ src/main/java/com/pafolder/graduation/controller/UIController.java
@@ -1,12 +1,23 @@
 package com.pafolder.graduation.controller;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UIController {
     @GetMapping("/")
-    public String login() {
+    public String root() {
         return "login";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam @Nullable String error, Model model) {
+        if (error != null) {
+            model.addAttribute("error", "User not found");
+        }
+            return "login";
     }
 }
